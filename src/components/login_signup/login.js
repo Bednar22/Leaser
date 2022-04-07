5cd4087f387c52cf18f21c1ca86d81c61bcf6577
Maciek Bednarski <mBednar2020@gmail.com>
2022-04-07 21:11:03 +0200

    added react hook form and done login error handling

--- src/components/login_signup/login.js
+++ src/components/login_signup/login.js
@@ -2,49 +2,100 @@ import React, { useState } from 'react';
 import { Paper, TextField, Grid, Container, Button, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { GridBreak } from '../utilities/gridBreak';
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import axios from 'axios';
 
 export const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const [error, setError] = useState(false);
+    const navigate = useNavigate();
 
-    const handleLogin = () => {
-        console.log('Test button click');
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const handleLogin = (data) => {
+        console.log(data);
+
+        axios
+            .post('/api/login', { data })
+            .then((res) => {
+                navigate('/');
+            })
+            .catch((err) => {
+                setError(true);
+            });
     };
 
     return (
         <>
             <Container maxWidth='xs'>
                 <Paper sx={{ p: 4 }}>
-                    <Grid container direction='row' justifyContent='center' alignItems='center' spacing={4}>
-                        <GridBreak />
-                        <Grid item sm={10} md={8}>
-                            <TextField label='E-mail adress' onChange={(e) => setUsername(e.target.value)}></TextField>
-                        </Grid>
-                        <GridBreak />
-                        <GridBreak />
-                        <Grid item sm={8} md={8}>
-                            <TextField
-                                label='Password'
-                                type='password'
-                                onChange={(e) => setPassword(e.target.value)}
-                            ></TextField>
-                        </Grid>
-                        <GridBreak />
+                    <form onSubmit={handleSubmit(handleLogin)}>
+                        <Grid container direction='row' justifyContent='center' spacing={4}>
+                            <GridBreak />
+                            <Grid item xs={10} md={10}>
+                                <TextField
+                                    fullWidth
+                                    label='E-mail adress'
+                                    type='email'
+                                    {...register('email', {
+                                        required: 'Email required',
+                                        pattern: {
+                                            value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                            message: 'Invalid email adress',
+                                        },
+                                    })}
+                                    error={errors.email ? true : false}
+                                    helperText={errors.email ? errors.email.message : null}
+                                    onFocus={() => setError(false)}
+                                ></TextField>
+                            </Grid>
+                            <GridBreak />
+                            <GridBreak />
+                            <Grid item xs={10} md={10}>
+                                <TextField
+                                    fullWidth
+                                    label='Password'
+                                    type='password'
+                                    onFocus={() => setError(false)}
+                                    {...register('password', {
+                                        required: 'Password required',
+                                    })}
+                                    error={errors.password ? true : false}
+                                ></TextField>
+                            </Grid>
+                            <GridBreak />
 
-                        <GridBreak />
-                        <Grid item sm={8} md={10}>
-                            <Typography variant='p'>
-                                Don't have an account? <Link to='/signup'> Join Leaser!</Link>
-                            </Typography>
+                            <Grid item xs={8} md={8} sx={{ mt: -3, mb: -1 }}>
+                                <Typography variant='subtitle2' color='error' align='center'>
+                                    {error ? 'Wrong e-mail or password' : null}
+                                </Typography>
+                            </Grid>
+
+                            <GridBreak />
+                            {
+                                <Grid item sm={8} md={10}>
+                                    <Typography variant='p'>
+                                        Don't have an account? <Link to='/signup'> Join Leaser!</Link>
+                                    </Typography>
+                                </Grid>
+                            }
+                            <GridBreak />
+                            <Grid item sm={8} md={8}>
+                                <Button
+                                    // onClick={handleSubmit(handleLogin)}
+                                    variant='contained'
+                                    sx={{ width: 1 / 1 }}
+                                    type='submit'
+                                >
+                                    Login
+                                </Button>
+                            </Grid>
                         </Grid>
-                        <GridBreak />
-                        <Grid item sm={8} md={8}>
-                            <Button onClick={handleLogin} variant='contained' sx={{ width: 1 / 1 }}>
-                                Login
-                            </Button>
-                        </Grid>
-                    </Grid>
+                    </form>
                 </Paper>
             </Container>
         </>
--- src/components/login_signup/signUp.js
+++ src/components/login_signup/signUp.js
@@ -1,15 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Paper, TextField, Grid, Container, Button, Typography, Stack, Box } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { GridBreak } from '../utilities/gridBreak';
 import { PhoneNumberConfiramtion } from './phoneNumberConfirmation';
 import axios from 'axios';
-
-export function sleep(duration) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, duration);
-    });
-}
 
 export const SignUp = (props) => {
     const [email, setEmail] = useState('');
@@ -17,14 +11,28 @@ export const SignUp = (props) => {
     const [passwordConfirm, setPasswordConfirm] = useState('');
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
-
+    const [emailError, setEmailError] = useState(false);
     const [passwordError, setPasswordError] = useState(false);
+    const navigate = useNavigate();
 
     const passwordCheck = () => {
         if (password !== passwordConfirm && passwordConfirm !== '') {
             setPasswordError(true);
         } else {
             setPasswordError(false);
+        }
+    };
+
+    const validateEmail = (email) => {
+        if (email) {
+            const error = String(email)
+                .toLowerCase()
+                .match(
+                    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                );
+            setEmailError(!error);
+        } else {
+            setEmailError(false);
         }
     };
 
@@ -35,10 +43,18 @@ export const SignUp = (props) => {
             firstName: firstName,
             lastName: lastName,
         };
-        console.log(user);
+
+        axios
+            .post('/api/signup', { user })
+            .then((res) => {
+                navigate('/login');
+            })
+            .catch((err) => {
+                console.log('Errors');
+            });
+
         e.preventDefault();
     };
-
 
     return (
         <>
@@ -54,17 +70,23 @@ export const SignUp = (props) => {
                             <GridBreak />
                             <Grid item xs={12} md={6}>
                                 <TextField
+                                    error={emailError}
+                                    required
                                     label='E-mail adress'
                                     size='small'
+                                    type='email'
                                     fullWidth
                                     value={email}
                                     onChange={(e) => setEmail(e.target.value)}
+                                    onBlur={(e) => validateEmail(e.target.value)}
+                                    {...(emailError ? { helperText: "It's not valid e-mail adress" } : {})}
                                 ></TextField>
                             </Grid>
                             <GridBreak />
                             <GridBreak />
                             <Grid item xs={12} md={6}>
                                 <TextField
+                                    required
                                     label='Password'
                                     type='password'
                                     size='small'
@@ -77,6 +99,7 @@ export const SignUp = (props) => {
                             <GridBreak />
                             <Grid item xs={12} md={6}>
                                 <TextField
+                                    required
                                     label='Confirm password'
                                     type='password'
                                     size='small'
@@ -93,12 +116,14 @@ export const SignUp = (props) => {
                             <Grid item xs={12} md={6}>
                                 <Stack direction='row' spacing={2}>
                                     <TextField
+                                        required
                                         label='First name'
                                         size='small'
                                         value={firstName}
                                         onChange={(e) => setFirstName(e.target.value)}
                                     ></TextField>
                                     <TextField
+                                        required
                                         label='Last name'
                                         size='small'
                                         value={lastName}
