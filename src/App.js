6f3c2c5fb3596a83381a5d28e5a9a87647a21427
Aleksander Górecki <goreckialeksander9@gmail.com>
2022-04-20 22:47:54 +0200

    added a mui theme containing chosen colors

--- src/App.js
+++ src/App.js
@@ -11,21 +11,38 @@ import { Homepage } from './components/homepage/homepage';
 import { NoMatch } from './components/noMatch';
 import { Profile } from './components/user_profile/profile';
 import { UserSettings } from './components/user_profile/userSettings';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#7ec3a6',
+            contrastText: "#fff"
+        },
+        secondary: {
+            main: '#ffa89a',
+            contrastText: "#fff"
+        },
+    },
+});
+
 /*podstawowy component, w ktorym beda sciezki  */
 function App() {
     const location = useLocation();
     return (
         <>
-            {location.pathname === '/' ? null : <Navbar></Navbar>}
-            <Routes>
-                <Route path='/' element={<Startpage />} />
-                <Route path='home' element={<Homepage />} />
-                <Route path='login' element={<Login />} />
-                <Route path='signup' element={<SignUp />} />
-                <Route path='offers' element={<MainOffersPage />} />
-                <Route path='user/settings' element={<UserSettings />} />
-                <Route path='user/profile' element={<Profile />} />
-            </Routes>
+            <ThemeProvider theme={theme}>
+                {location.pathname === '/' ? null : <Navbar></Navbar>}
+                <Routes>
+                    <Route path='/' element={<Startpage />} />
+                    <Route path='home' element={<Homepage />} />
+                    <Route path='login' element={<Login />} />
+                    <Route path='signup' element={<SignUp />} />
+                    <Route path='offers' element={<MainOffersPage />} />
+                    <Route path='user/settings' element={<UserSettings />} />
+                    <Route path='user/profile' element={<Profile />} />
+                </Routes>
+            </ThemeProvider>
         </>
     );
 }
