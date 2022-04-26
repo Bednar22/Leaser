e0379aa04a03236daa3311f994b2da41b40c8725
Maciek Bednarski <mBednar2020@gmail.com>
2022-04-26 17:42:31 +0200

    working addOffer component

--- src/components/offers/addOffer.js
+++ src/components/offers/addOffer.js
@@ -1,31 +1,68 @@
 import React, { useState, useEffect } from 'react';
 import { Paper, TextField, Grid, Container, Button, Typography, Stack, Box } from '@mui/material';
 import { GridBreak } from '../utilities/gridBreak';
-import { useForm, Controller } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
 import axios from 'axios';
 import '../../App.css';
 import { InputLabel } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 const Input = styled('input')({
     display: 'none',
 });
 
-const validationSchema = yup.object().shape({
-    //maybe schema
-});
-
 export const AddOffer = (props) => {
+    const [title, setTitle] = useState();
+    const [description, setDescription] = useState('');
+    const [pricePerDay, setPricePerDay] = useState('');
+    const [pricePerWeek, setPricePerWeek] = useState('');
+    const [pricePerMonth, setPricePerMonth] = useState('');
+    const [deposit, setDeposit] = useState('');
+    const [availableFrom, setAvailableFrom] = useState('');
+    const [availableTo, setAvailableTo] = useState('');
     const [selectedImage, setSelectedImage] = useState(null);
     const smallSize = useMediaQuery('(max-width:900px)');
+
+    const addOffer = (e) => {
+        let formData = new FormData();
+        formData.append('title', title);
+        formData.append('description', description);
+        formData.append('pricePerDay', pricePerDay);
+        formData.append('pricePerWeek', pricePerWeek);
+        formData.append('pricePerMonth', pricePerMonth);
+        formData.append('depositId', '');
+        formData.append('availableFrom', availableFrom.toISOString());
+        formData.append('availableTo', availableTo.toISOString());
+        formData.append('postImage', selectedImage);
+
+        const config = {
+            headers: {
+                'content-type': 'multipart/form-data',
+                Authorization: `Bearer ${window.localStorage.getItem('leaserToken')}`,
+            },
+        };
+        const categoryId = 1;
+
+        axios
+            .post(`/api/Posts/${categoryId}`, formData, config)
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((error) => {
+                console.log(error.response);
+            });
+
+        e.preventDefault();
+    };
+
     return (
         <>
             <Container maxWidth='lg'>
                 <Paper sx={{ p: 2, mb: 6 }}>
-                    <form onSubmit={() => alert('Submit')}>
+                    <form onSubmit={(e) => addOffer(e)}>
                         <Box
                             sx={
                                 smallSize
@@ -35,7 +72,15 @@ export const AddOffer = (props) => {
                         >
                             <Grid container spacing={{ xs: 2, md: 3 }} direction='row' alignItems='flex-start'>
                                 <Grid item xs={12} md={12}>
-                                    <TextField required label='Item' size='small' type='text' fullWidth></TextField>
+                                    <TextField
+                                        required
+                                        label='Title'
+                                        size='small'
+                                        type='text'
+                                        fullWidth
+                                        value={title}
+                                        onChange={(e) => setTitle(e.target.value)}
+                                    ></TextField>
                                 </Grid>
                                 <Grid item xs={12} md={12}>
                                     <TextField
@@ -46,6 +91,8 @@ export const AddOffer = (props) => {
                                         type='text'
                                         size='small'
                                         fullWidth
+                                        value={description}
+                                        onChange={(e) => setDescription(e.target.value)}
                                     ></TextField>
                                 </Grid>
 
@@ -57,6 +104,8 @@ export const AddOffer = (props) => {
                                         type='text'
                                         size='small'
                                         fullWidth
+                                        value={pricePerDay}
+                                        onChange={(e) => setPricePerDay(e.target.value)}
                                     ></TextField>
                                 </Grid>
 
@@ -67,6 +116,8 @@ export const AddOffer = (props) => {
                                         type='text'
                                         size='small'
                                         fullWidth
+                                        value={pricePerWeek}
+                                        onChange={(e) => setPricePerWeek(e.target.value)}
                                     ></TextField>
                                 </Grid>
 
@@ -77,18 +128,48 @@ export const AddOffer = (props) => {
                                         type='text'
                                         size='small'
                                         fullWidth
+                                        value={pricePerMonth}
+                                        onChange={(e) => setPricePerMonth(e.target.value)}
                                     ></TextField>
                                 </Grid>
 
                                 <Grid item xs={6} md={6}>
                                     <TextField
-                                        // required
                                         label='Deposit'
                                         type='text'
                                         size='small'
                                         fullWidth
+                                        value={deposit}
+                                        onChange={(e) => setDeposit(e.target.value)}
                                     ></TextField>
                                 </Grid>
+
+                                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                    <Grid item xs={6}>
+                                        <DatePicker
+                                            // inputFormat='dd.mm.yyyy'
+                                            label='Available from'
+                                            value={availableFrom}
+                                            onChange={(newValue) => {
+                                                setAvailableFrom(newValue);
+                                            }}
+                                            renderInput={(params) => <TextField {...params} />}
+                                            minDate={Date.now()}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={6}>
+                                        <DatePicker
+                                            // inputFormat='dd.mm.yyyy'
+                                            label='Available to'
+                                            value={availableTo}
+                                            onChange={(newValue) => {
+                                                setAvailableTo(newValue);
+                                            }}
+                                            minDate={availableFrom}
+                                            renderInput={(params) => <TextField {...params} />}
+                                        />
+                                    </Grid>
+                                </LocalizationProvider>
                             </Grid>
                         </Box>
                         <Box
