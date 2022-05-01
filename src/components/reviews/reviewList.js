08a0da6d6aae5edd44633e60039575f5acb4be13
Aleksander Górecki <goreckialeksander9@gmail.com>
2022-05-01 18:25:23 +0200

    added a button showing more reviews on click

--- src/components/reviews/reviewList.js
+++ src/components/reviews/reviewList.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { Paper, Typography, Stack, Button, Box } from '@mui/material';
+import { Paper, Typography, Stack, Button, Box, Container } from '@mui/material';
 import { Review } from './review';
 
 
@@ -11,20 +11,35 @@ export const ReviewList = ( {reviews} ) => {
 
     const reviewList = []
     for (let i = 0; i < 13; i++) {
-        reviewList.push({reviewAuthor: 'reviewAuthor', reviewContents: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at consectetur purus. Sed sit amet ligula mattis, posuere nulla vitae, dapibus lorem. Vestibulum quis nunc et est interdum facilisis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam enim odio, porttitor id pretium non, rutrum at nisl. Pellentesque faucibus risus quis orci dapibus, eget rutrum sem congue. Donec ullamcorper ut nibh a vulputate. Integer neque lacus, congue sed ex in, pharetra dapibus lorem.', reviewScore: 4.5})
+        reviewList.push({reviewAuthor: 'reviewAuthor', reviewContents: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at consectetur purus. Sed sit amet ligula mattis, posuere nulla vitae, dapibus lorem.', reviewScore: 4.5})
     }
 
-    const [displayedReviewsCount, setDisplayedReviewsCount] = useState(0);
+    const [displayedReviewsCount, setDisplayedReviewsCount] = useState(maxReviewsAtOnce);
 
-    // const displayMoreButtonText = () => {
-    //     if (reviews.Count() - displayedReviewsCount) 
-    // }
+    const ShowMoreReviewsButton = () => {
+
+        const showMoreReviewsOnClick = () => {
+            setDisplayedReviewsCount(displayedReviewsCount + maxReviewsAtOnce)
+        }
+
+        if (displayedReviewsCount < reviewList.length) {
+            return (
+                <Button variant='contained' sx={{m: 1}} onClick={showMoreReviewsOnClick}>
+                    Show more reviews  
+                </Button>
+            )
+        }
+        else {
+            return null;
+        }
+    }
 
     return (
         <>
+        <Container>
         <Stack>
             <Stack>
-                {reviewList.slice(0, maxReviewsAtOnce).map((item) => {
+                {reviewList.slice(0, displayedReviewsCount).map((item) => {
                     return (
                         <Box sx={{m: 1}}>
                             <Review
@@ -36,10 +51,9 @@ export const ReviewList = ( {reviews} ) => {
                     )
                 })}
             </Stack>
-            <Button variant='contained' sx={{m: 1, mt: 3}}>
-                Show {maxReviewsAtOnce} more reviews  
-            </Button>
+            <ShowMoreReviewsButton />
         </Stack>
+        </Container>
         </>
     )
 }
