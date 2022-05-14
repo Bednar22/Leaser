a952ce29f7d829444e4cbee28970431c9dc53b4d
Klaudia Teodorowicz <74158485+k-teo@users.noreply.github.com>
2022-05-14 15:15:52 +0200

    get categories froim database

--- src/components/homepage/categoriesTiles.js
+++ src/components/homepage/categoriesTiles.js
@@ -1,58 +1,11 @@
 import { ImageList, ImageListItem, ImageListItemBar, Grid } from '@mui/material';
 import '../../App.css';
 import { useNavigate } from 'react-router-dom';
-const categories = [
-    {
-        img: 'https://images.unsplash.com/photo-1542060748-10c28b62716f',
-        title: 'Clothes',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1512412046876-f386342eddb3',
-        title: 'Sports',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1502982720700-bfff97f2ecac',
-        title: 'Cameras',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1563090728-6f7cdbfb4d72',
-        title: 'Tools',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1603398938378-e54eab446dde',
-        title: 'Medicine',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1521405924368-64c5b84bec60',
-        title: 'Drones',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1621460248083-6271cc4437a8',
-        title: 'Garden',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1615655406736-b37c4fabf923',
-        title: 'Electronic',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1558060370-d644479cb6f7',
-        title: 'Toys',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1632501641765-e568d28b0015',
-        title: 'Games',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1570102881689-c04ab4cf1f4c',
-        title: 'Parties',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1571068316344-75bc76f77890',
-        title: 'Bikes',
-    },
-];
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export const CategoriesTiles = (props) => {
+    const [categories, setCategories] = useState([{}]);
     const navigate = useNavigate();
 
     const cateogoryClick = (category) => {
@@ -61,6 +14,22 @@ export const CategoriesTiles = (props) => {
 
     const tile_size = 244;
 
+    useEffect(() => {
+        axios
+            .get('api/Categories', {
+                headers: {
+                    Authorization: `Bearer ${window.localStorage.getItem('leaserToken')}`,
+                },
+            })
+            .then((res) => {
+                console.log(res.data);
+                setCategories(res.data);
+            })
+            .catch((err) => {});
+    }, []);
+
+    console.log(categories);
+
     return (
         <>
             <Grid container justifyContent='center'>
@@ -68,17 +37,17 @@ export const CategoriesTiles = (props) => {
                     <ImageList cols={4}>
                         {categories.map((item) => (
                             <ImageListItem
-                                key={item.img}
-                                onClick={() => cateogoryClick(item.title)}
+                                key={item.id}
+                                onClick={() => cateogoryClick(item.categoryName)}
                                 className='category-tile'
                             >
                                 <img
-                                    src={`${item.img}?w=${tile_size}&h=${tile_size}&fit=crop&auto=format`}
-                                    srcSet={`${item.img}?w=${tile_size}&h=${tile_size}&fit=crop&auto=format&dpr=2 2x`}
-                                    alt={item.title}
+                                    src={`${item.imageURL}?w=${tile_size}&h=${tile_size}&fit=crop&auto=format`}
+                                    srcSet={`${item.imageURL}?w=${tile_size}&h=${tile_size}&fit=crop&auto=format&dpr=2 2x`}
+                                    alt={item.categoryName}
                                     loading='lazy'
                                 />
-                                <ImageListItemBar title={item.title} />
+                                <ImageListItemBar title={item.categoryName} />
                             </ImageListItem>
                         ))}
                     </ImageList>
