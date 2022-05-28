2311eb256ec9b8589f2b1b029c84ef6e0931f593
Aleksander Górecki <goreckialeksander9@gmail.com>
2022-05-28 22:12:33 +0200

    changes before state management rework

--- src/components/offers/offerDetails.js
+++ src/components/offers/offerDetails.js
@@ -8,7 +8,6 @@ import { NavLink } from 'react-router-dom';
 import { CalendarPicker } from '@mui/x-date-pickers';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import AdapterDateFns from '@mui/lab/AdapterDateFns'
-import { SettingsApplicationsRounded } from '@mui/icons-material';
 
 
 export const OfferDetails = () => {
@@ -33,6 +32,8 @@ export const OfferDetails = () => {
     const [previousTransactions, setPreviousTransactions] = useState(null);
 
     const [dateDisableFunction, setDateDisableFunction] = useState(null);
+
+    const [groupedResponseData, setgroupedResponseData] = useState(null);
     const [loaded, setLoaded] = useState(false);
 
     const config = {
@@ -113,99 +114,120 @@ export const OfferDetails = () => {
         }
     }, [availableFrom, availableTo, previousTransactions])
 
-    useEffect(()=>{
-        console.log(dateDisableFunction)
-    }, [dateDisableFunction])
+
+    const TopLeftPanel = () => {
+        return (
+            <img src={offerImage} alt={offerTitle} height='100%' width='100%' style={{objectFit: 'cover', borderRadius: '4px'}}/>
+        );
+    }
+
+    const TopRightPanel = () => {
+        return (
+            <Stack spacing={1} height='100%'>
+                <Paper style={{ flex: '0.5', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                    <Stack p={1} alignItems='center'>
+                        <Typography variant='h6' sx={{wordWrap: 'break-word'}}>
+                            {renterNickname}
+                        </Typography>
+                        <Rating readOnly precision={0.1} value={renterScore}/>
+                    </Stack>
+                </Paper>
+                <Stack spacing={1} style={{ flex: '1' }}>
+                    <Paper style={{ flex: '1', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                        <Stack direction='row' spacing={5} justifyContent='center' alignContent='center'>
+                            <Stack justifyContent='center' alignItems='center'>
+                                <Typography color='secondary' fontWeight='bold' variant='h6'>
+                                    Day+
+                                </Typography>
+                                <Typography textAlign='center'>
+                                    {pricePerDay} points/day
+                                </Typography>
+                            </Stack>
+                            <Stack justifyContent='center' alignItems='center'>
+                                <Typography color='secondary' fontWeight='bold' variant='h6'>
+                                    Week+
+                                </Typography>
+                                <Typography textAlign='center'>
+                                    {pricePerWeek} points/day
+                                </Typography>
+                            </Stack>
+                            <Stack justifyContent='center' alignItems='center'>
+                                <Typography color='secondary' fontWeight='bold' variant='h6'>
+                                    Month+
+                                </Typography>
+                                <Typography textAlign='center'>
+                                    {pricePerMonth} points/day
+                                </Typography>
+                            </Stack>
+                        </Stack>
+                    </Paper>
+                    <Paper style={{ flex: '1', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                        <Stack justifyContent='center' alignItems='center'>
+                            <Typography variant='h6' color='secondary' fontWeight='bold'>
+                                Deposit
+                            </Typography>
+                            <Typography>
+                                {deposit} points
+                            </Typography>
+                        </Stack>
+                    </Paper>
+                    <Button variant='contained' component={NavLink} to='booking'>
+                        Rent this item
+                    </Button>
+                </Stack>
+            </Stack>
+        );
+    }
+
+    const BottomLeftPanel = () => {
+        return (
+            <Paper>
+                <Stack p={1}>
+                    <Typography variant='h5' fontWeight='bold' sx={{wordWrap: 'break-word'}}>
+                        {offerTitle}
+                    </Typography>
+                    <Typography variant='h6' fontWeight='bold' color='secondary' sx={{wordWrap: 'break-word'}}>
+                        In {offerCity}
+                    </Typography>
+                    <Typography sx={{wordWrap: 'break-word'}}>
+                        {offerDescription}
+                    </Typography>
+                </Stack>
+            </Paper>
+        );
+    }
+
+    const BottomRightPanel = () => {
+        return (
+            <Paper style={{ dispaly: 'flex', alignItems: 'center' }}>
+                <Stack p={1} alignItems='center'>
+                    <Typography variant='h6'>
+                        Availability
+                    </Typography>
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <CalendarPicker shouldDisableDate={dateDisableFunction} minDate={Date.now()} maxDate={availableTo} onChange={function dummy() {}} sx={{minHeight: '100px'}}/>
+                    </LocalizationProvider>
+                </Stack>
+            </Paper>
+        );
+    }
     
     return (
         <>
             <Container maxWidth='xl'>
                 <Grid container justifyContent='center' alignContent='center' spacing={2}>
                     <Grid item xs={9} md={6} lg={5} height='350px'>
-                        <img src={offerImage} alt={offerTitle} height='100%' width='100%' style={{objectFit: 'cover', borderRadius: '4px'}}/>
+                        {loaded ? <TopLeftPanel/> : <Skeleton/> }
                     </Grid>
                     <Grid item xs={9} md={5} lg={4} height='350px'>
-                        <Stack spacing={1} height='100%'>
-                            <Paper style={{ flex: '0.5', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                                <Stack p={1} alignItems='center'>
-                                    <Typography variant='h6' sx={{wordWrap: 'break-word'}}>
-                                        {renterNickname}
-                                    </Typography>
-                                    <Rating readOnly precision={0.1} value={renterScore}/>
-                                </Stack>
-                            </Paper>
-                            <Stack spacing={1} style={{ flex: '1' }}>
-                                <Paper style={{ flex: '1', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                                    <Stack direction='row' spacing={5} justifyContent='center' alignContent='center'>
-                                        <Stack justifyContent='center' alignItems='center'>
-                                            <Typography color='secondary' fontWeight='bold' variant='h6'>
-                                                Day+
-                                            </Typography>
-                                            <Typography textAlign='center'>
-                                                {pricePerDay} points/day
-                                            </Typography>
-                                        </Stack>
-                                        <Stack justifyContent='center' alignItems='center'>
-                                            <Typography color='secondary' fontWeight='bold' variant='h6'>
-                                                Week+
-                                            </Typography>
-                                            <Typography textAlign='center'>
-                                                {pricePerWeek} points/day
-                                            </Typography>
-                                        </Stack>
-                                        <Stack justifyContent='center' alignItems='center'>
-                                            <Typography color='secondary' fontWeight='bold' variant='h6'>
-                                                Month+
-                                            </Typography>
-                                            <Typography textAlign='center'>
-                                                {pricePerMonth} points/day
-                                            </Typography>
-                                        </Stack>
-                                    </Stack>
-                                </Paper>
-                                <Paper style={{ flex: '1', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                                    <Stack justifyContent='center' alignItems='center'>
-                                        <Typography variant='h6' color='secondary' fontWeight='bold'>
-                                            Deposit
-                                        </Typography>
-                                        <Typography>
-                                            {deposit} points
-                                        </Typography>
-                                    </Stack>
-                                </Paper>
-                                <Button variant='contained' component={NavLink} to='booking'>
-                                    Rent this item
-                                </Button>
-                            </Stack>
-                        </Stack>
+                        {loaded ? <TopRightPanel/> : <Skeleton/> }
                     </Grid>
                     <GridBreak></GridBreak>
                     <Grid item xs={9} md={6} lg={5}>
-                        <Paper>
-                            <Stack p={1}>
-                                <Typography variant='h5' fontWeight='bold' sx={{wordWrap: 'break-word'}}>
-                                    {offerTitle}
-                                </Typography>
-                                <Typography variant='h6' fontWeight='bold' color='secondary' sx={{wordWrap: 'break-word'}}>
-                                    In {offerCity}
-                                </Typography>
-                                <Typography sx={{wordWrap: 'break-word'}}>
-                                    {offerDescription}
-                                </Typography>
-                            </Stack>
-                        </Paper>
+                        {loaded ? <BottomLeftPanel/> : <Skeleton/> }
                     </Grid>
                     <Grid item xs={9} md={5} lg={4}>
-                        <Paper style={{ dispaly: 'flex', alignItems: 'center' }}>
-                            <Stack p={1} alignItems='center'>
-                                <Typography variant='h6'>
-                                    Availability
-                                </Typography>
-                                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                                    <CalendarPicker shouldDisableDate={dateDisableFunction} minDate={Date.now()} maxDate={availableTo} onChange={function dummy() {}} sx={{minHeight: '100px'}}/>
-                                </LocalizationProvider>
-                            </Stack>
-                        </Paper>
+                        {loaded ? <BottomRightPanel/> : <Skeleton/> }
                     </Grid>
                 </Grid>
             </Container>
