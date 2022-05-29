74ffda1e3ca8e428a1e68659cd234a7be2cd6bba
Aleksander Górecki <goreckialeksander9@gmail.com>
2022-05-29 17:04:31 +0200

    initial version of adding a transaction to the database

--- src/components/offers/booking.js
+++ src/components/offers/booking.js
@@ -1,11 +1,10 @@
 import { Grid, Container, Paper, Typography, Stack, Button, Box, TextField, Skeleton } from '@mui/material';
 import React, { useState, useEffect } from 'react';
-import { NavLink, useParams } from 'react-router-dom';
+import { NavLink, useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import AdapterDateFns from '@mui/lab/AdapterDateFns'
-import { GridBreak } from '../utilities/gridBreak';
 
 
 
@@ -13,6 +12,8 @@ export const Booking = () => {
 
     const params = useParams();
     const offerId = params.id;
+
+    const navigate = useNavigate();
 
     const [rentFrom, setRentFrom] = useState(null);
     const [rentTo, setRentTo] = useState(null);
@@ -24,7 +25,6 @@ export const Booking = () => {
     const [availableTo, setAvailableTo] = useState(null);
     const [previousTransactions, setPreviousTransactions] = useState(null);
 
-    const [allDataLoaded, setAllDataLoaded] = useState(false);
     const [rentFromDateDisableFunction, setRentFromDateDisableFunction] = useState(null);
     const [rentToDateDisableFunction, setRentToDateDisableFunction] = useState(null);
 
@@ -32,6 +32,9 @@ export const Booking = () => {
     const [total, setTotal] = useState(null);
     const [fromError, setFromError] = useState(null);
     const [toError, setToError] = useState(null);
+    const [transactionError, setTransactionError] = useState(null);
+
+    const [currentUserId, setcurrentUserId] = useState(null);
 
 
     const config = {
@@ -66,10 +69,17 @@ export const Booking = () => {
                 console.log(error)
             });
 
-    }, [])
+        axios
+            .get(`/api/Accounts/User`, config)
+            .then( (res) => {
+                setcurrentUserId(res.data.id);
+                console.log(res.data.nickName);
+            })
+            .catch((error) => {
+                console.log(error)
+            });
 
-    useEffect( () => {
-    }, [allDataLoaded, pricePerDay, pricePerWeek, pricePerMonth, deposit, availableFrom, availableTo, previousTransactions])
+    }, [])
 
 
     useEffect(() => {
@@ -179,119 +189,157 @@ export const Booking = () => {
         }
     }, [availableFrom, availableTo, previousTransactions, rentFrom, rentTo, fromError])
 
+    const handleTransaction = () => {
+
+        if (fromError || toError) {
+            setTransactionError('Provided renting dates are not correct.')
+            return;
+        }
+
+        if (rentFrom == null || rentTo == null) {
+            setTransactionError('Please fill out both renting dates before confirmation and payment.')
+            return;
+        }
+
+        let data = {
+            postId: offerId,
+            payerId: currentUserId,
+            price: total,
+            dateFrom: rentFrom,
+            dateTo: rentTo
+        }
+
+        axios
+        .post('/api/Transactions', data, config)
+        .then((res) => {
+            console.log(res);
+            setTransactionError(null);
+            navigate(`/offers/offerDetails/${offerId}`);
+        })
+        .catch((error) => {
+            setTransactionError(error.response.data);
+        });
+    }
+
     return (
         <>
         <Container maxWidth='sm'>
             <Paper>
-                <form>
-                    <Box p={3}>
-                        <Grid container justifyContent='center' alignContent='center' spacing={{ xs: 1, sm: 3 }}>
-                            <Grid item xs={12} sm={6}>
-                                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                                    <Stack justifyContent='center' alignContent='center' spacing={1}>
-                                        {rentFromDateDisableFunction ? (
-                                            <DatePicker 
-                                                label='Rent from'
-                                                value={rentFrom}
-                                                onChange={(newValue) => {
-                                                    setRentFrom(newValue);
-                                                }}
-                                                renderInput={(params) => <TextField {...params} size='small'/>}
-                                                minDate={new Date()}
-                                                maxDate={ rentTo != null ? getPreviousDayFromDate(rentTo) : getNextYearFromDate(new Date())}
-                                                shouldDisableDate={rentFromDateDisableFunction}
-                                                clearable={true}
-                                                onError={(error)=>{setFromError(error)}}
-                                                onAccept={() => { setFromError(null) }}
-                                                defaultCalendarMonth={rentTo ? rentTo : null}
-                                            />
-                                        ) : (
-                                            <Skeleton variant='rectangular' height='40px'/>
-                                        )}
-                                        {rentToDateDisableFunction ? (
-                                            <DatePicker
-                                            label='Rent to'
-                                            value={rentTo}
+                <Box p={3}>
+                    <Grid container justifyContent='center' alignContent='center' spacing={{ xs: 1, sm: 3 }}>
+                        <Grid item xs={12} sm={6}>
+                            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                <Stack justifyContent='center' alignContent='center' spacing={1}>
+                                    {rentFromDateDisableFunction ? (
+                                        <DatePicker 
+                                            label='Rent from'
+                                            value={rentFrom}
                                             onChange={(newValue) => {
-                                                setRentTo(newValue);
+                                                setRentFrom(newValue);
                                             }}
-                                            minDate={rentFrom != null ? getNextDayFromDate(rentFrom): getNextDayFromDate(new Date())}
-                                            maxDate={getNextYearFromDate(new Date())}
-                                            shouldDisableDate={rentToDateDisableFunction}
                                             renderInput={(params) => <TextField {...params} size='small'/>}
+                                            minDate={new Date()}
+                                            maxDate={ rentTo != null ? getPreviousDayFromDate(rentTo) : getNextYearFromDate(new Date())}
+                                            shouldDisableDate={rentFromDateDisableFunction}
                                             clearable={true}
-                                            onError={(error)=>{setToError(error)}}
-                                            onAccept={() => { setToError(null) }}
-                                            defaultCalendarMonth={rentFrom ? rentFrom : null}
+                                            onError={(error)=>{setFromError(error)}}
+                                            onAccept={() => { setFromError(null) }}
+                                            defaultCalendarMonth={rentTo ? rentTo : null}
                                         />
-                                        ) : (
-                                            <Skeleton variant='rectangular' height='40px'/>
-                                        )}
-                                    </Stack>  
-                                </LocalizationProvider>
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                {(deposit != null && pricePerDay != null && pricePerWeek != null && pricePerMonth != null) ? (
-                                    <Grid container spacing={1}>
-                                        <Grid item xs={4} sm={5}>
-                                            <Typography fontWeight='bold'>
-                                                Deposit
-                                            </Typography>
-                                        </Grid>
-                                        <Grid item xs={6} sm={5}>
-                                            <Typography>
-                                                {deposit != null ? (deposit + ' points') : null}
-                                            </Typography> 
-                                        </Grid>
-                                        <Grid item xs={4} sm={5}>
-                                            <Typography fontWeight='bold'>
-                                                Price
-                                            </Typography>
-                                        </Grid>
-                                        <Grid item xs={6}>
-                                            <Typography sx={{ pr: 1 }}>
-                                                {rentingPrice != null ? (rentingPrice + ' points') : null}
-                                            </Typography>
-                                        </Grid>
-                                        <Grid item xs={4} sm={5}>
-                                            <Typography variant='h6' fontWeight='bold'>
-                                                Total
-                                            </Typography>
-                                        </Grid>
-                                        <Grid item xs={6}>
-                                            <Typography variant='h6'>
-                                                {total != null ? (total + ' points') : null}
-                                            </Typography>
-                                        </Grid>
-                                    </Grid>
-                                ) : (
-                                    <Skeleton variant='rectangular' height='100%'/>
-                                )}
-                            </Grid>
-                            <GridBreak/>
-                            <Grid item xs={12} sm={4}>
-                                {(rentFromDateDisableFunction && rentToDateDisableFunction) ? 
-                                (
-                                    <Button variant='outlined' color='secondary' component={NavLink} to={`/offers/offerDetails/${offerId}`} fullWidth>
-                                        Cancel
-                                    </Button>
-                                ) : (
-                                    <Skeleton variant='rectangular' height='40px'/>
-                                )}
-                            </Grid>
-                            <Grid item xs={12} sm={8}>
-                                {(rentFromDateDisableFunction && rentToDateDisableFunction) ?
-                                (
-                                    <Button variant='contained' fullWidth>
-                                        Confirm and pay
-                                    </Button>
-                                ) : (
-                                    <Skeleton variant='rectangular' height='40px'/>
-                                )}
-                            </Grid>
+                                    ) : (
+                                        <Skeleton variant='rectangular' height='40px'/>
+                                    )}
+                                    {rentToDateDisableFunction ? (
+                                        <DatePicker
+                                        label='Rent to'
+                                        value={rentTo}
+                                        onChange={(newValue) => {
+                                            setRentTo(newValue);
+                                        }}
+                                        minDate={rentFrom != null ? getNextDayFromDate(rentFrom): getNextDayFromDate(new Date())}
+                                        maxDate={getNextYearFromDate(new Date())}
+                                        shouldDisableDate={rentToDateDisableFunction}
+                                        renderInput={(params) => <TextField {...params} size='small'/>}
+                                        clearable={true}
+                                        onError={(error)=>{setToError(error)}}
+                                        onAccept={() => { setToError(null) }}
+                                        defaultCalendarMonth={rentFrom ? rentFrom : null}
+                                    />
+                                    ) : (
+                                        <Skeleton variant='rectangular' height='40px'/>
+                                    )}
+                                </Stack>  
+                            </LocalizationProvider>
                         </Grid>
-                    </Box>
-                </form>
+                        <Grid item xs={12} sm={6}>
+                            {(deposit != null && pricePerDay != null && pricePerWeek != null && pricePerMonth != null) ? (
+                                <Grid container spacing={1}>
+                                    <Grid item xs={4} sm={5}>
+                                        <Typography fontWeight='bold'>
+                                            Deposit
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={6} sm={5}>
+                                        <Typography>
+                                            {deposit != null ? (deposit + ' points') : null}
+                                        </Typography> 
+                                    </Grid>
+                                    <Grid item xs={4} sm={5}>
+                                        <Typography fontWeight='bold'>
+                                            Price
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={6}>
+                                        <Typography sx={{ pr: 1 }}>
+                                            {rentingPrice != null ? (rentingPrice + ' points') : null}
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={4} sm={5}>
+                                        <Typography variant='h6' fontWeight='bold'>
+                                            Total
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={6}>
+                                        <Typography variant='h6'>
+                                            {total != null ? (total + ' points') : null}
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                            ) : (
+                                <Skeleton variant='rectangular' height='100%'/>
+                            )}
+                        </Grid>
+                        {(transactionError ? (
+                            <Grid item xs={12} sm={12}>
+                                <Typography color='error' variant='subtitle2' align='center'>
+                                    {transactionError}
+                                </Typography>
+                            </Grid>
+                        ) : (
+                            null
+                        ))}
+                        <Grid item xs={12} sm={4}>
+                            {(rentFromDateDisableFunction && rentToDateDisableFunction) ? 
+                            (
+                                <Button variant='outlined' color='secondary' component={NavLink} to={`/offers/offerDetails/${offerId}`} fullWidth>
+                                    Cancel
+                                </Button>
+                            ) : (
+                                <Skeleton variant='rectangular' height='40px'/>
+                            )}
+                        </Grid>
+                        <Grid item xs={12} sm={8}>
+                            {(rentFromDateDisableFunction && rentToDateDisableFunction && (currentUserId != null)) ?
+                            (
+                                <Button variant='contained' fullWidth onClick={handleTransaction}>
+                                    Confirm and pay
+                                </Button>
+                            ) : (
+                                <Skeleton variant='rectangular' height='40px'/>
+                            )}
+                        </Grid>
+                    </Grid>
+                </Box>
             </Paper>
         </Container>
         </>
