897f207e8475f40946d4b4e4d6be7118a55a743d
Maciek Bednarski <mBednar2020@gmail.com>
2022-05-30 12:38:44 +0200

    add points request

--- src/components/user_profile/addPoints.js
+++ src/components/user_profile/addPoints.js
@@ -6,11 +6,14 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import { Stack } from '@mui/material';
+import { useAuth } from '../utilities/auth';
 import axios from 'axios';
 
 export const AddPoints = (props) => {
-    const [open, setOpen] = React.useState(false);
-
+    const [open, setOpen] = useState(false);
+    const [pointsAmmount, setpointsAmmount] = useState(10);
+    const auth = useAuth();
     const handleClickOpen = () => {
         setOpen(true);
     };
@@ -19,30 +22,54 @@ export const AddPoints = (props) => {
         setOpen(false);
     };
 
+    const handleConfirm = () => {
+        const token = window.localStorage.getItem('leaserToken');
+        const config = {
+            headers: {
+                'content-type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        };
+
+        axios
+            .put(`/api/Accounts/${auth.user.id}/AddPoints`, pointsAmmount, config)
+            .then((res) => {
+                console.log(res.data);
+                handleClose();
+            })
+            .catch((error) => {
+                console.log(error.response);
+            });
+    };
+
     return (
         <>
             <Button variant='contained' color='secondary' onClick={handleClickOpen}>
                 Add points
             </Button>
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>Add points to your account</DialogTitle>
+            <Dialog open={open} onClose={handleClose} maxWidth='sm' fullWidth>
+                <DialogTitle>Choose ammount</DialogTitle>
                 <DialogContent>
-                    <DialogContentText>Choose ammount</DialogContentText>
                     <TextField
                         autoFocus
                         margin='dense'
-                        id='name'
+                        id='points'
                         label='Ammount'
-                        type='email'
+                        type='number'
                         fullWidth
                         variant='standard'
+                        onChange={(e) => setpointsAmmount(e.target.value)}
                     />
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleClose} color='secondary'>
-                        Cancel
-                    </Button>
-                    <Button onClick={handleClose}>Confirm</Button>
+                    <Stack direction='row' justifyContent='space-between' sx={{ width: 1 / 1, mx: 2, mb: 2 }}>
+                        <Button variant='outlined' onClick={handleClose} color='secondary'>
+                            Cancel
+                        </Button>
+                        <Button variant='outlined' onClick={handleConfirm}>
+                            Add points
+                        </Button>
+                    </Stack>
                 </DialogActions>
             </Dialog>
         </>
